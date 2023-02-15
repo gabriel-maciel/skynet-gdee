0488c8877c6ccf723ad7b10bf24cd0bb053f5ce3
Gabriel Maciel <g.maciel@buenosaires.gob.ar>
2023-02-15 12:11:02 -0300

    Update bot handlers

--- skynet-bot.py
+++ skynet-bot.py
@@ -1,20 +1,35 @@
 import telebot
+import datetime
+import threading
 import random
 
-# Inserta aquí el token de tu bot
 BOT_TOKEN = '5725350819:AAFya5pw6KGoce8koCzkSQdsi7EGFAkknt4'
-
 bot = telebot.TeleBot(BOT_TOKEN)
+mensajes_aleatorios = ["Y como vienen con las tareas chicos?", "Hola chicos, quería saber si tienen alguna duda", "Chicos, cómo venimos con los pendientes?", "Chicos, me pregunta Lord PM para cuando salimos a prod"]
 
-messages = ["Qué te pasa pelotudo?", "Qué agita gil?", "Bueno loco, mira que le digo al dog", "Pasame tu dni", "Saludos cordiales"]
+hora_inicio = datetime.time(10, 0, 0)
+hora_fin = datetime.time(17, 0, 0)
 
-@bot.message_handler(commands=['start', 'hello'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+@bot.message_handler(commands=['start'])
+def enviar_mensaje_de_bienvenida(message):
+    bot.reply_to(message, "¡Hola! Soy un bot de Telegram")
 
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    response = random.choice(messages)
-    bot.reply_to(message, response)
+@bot.message_handler(commands=['dogger'])
+def iniciar_temporizador(message):
+    t = threading.Thread(target=enviar_mensaje_programado)
+    t.start()
+    bot.send_message(chat_id=message.chat.id, text="Recibirás un mensaje del dog cada 2 a 3 horas dentro del rango horario permitido (10 AM a 5 PM).")
 
-bot.infinity_polling()
+def enviar_mensaje_programado():
+    while True:
+        hora_actual = datetime.datetime.now().time()
+        if hora_inicio <= hora_actual <= hora_fin:
+            tiempo_espera = random.randint(7200, 10800)
+            mensaje = random.choice(mensajes_aleatorios)
+            bot.send_message(chat_id, mensaje)
+            time.sleep(tiempo_espera)
+        else:
+            time.sleep(3600)
+
+# Inicia el bot
+bot.polling()
